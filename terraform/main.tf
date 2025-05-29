f53562176d237953816f52516646ac6ce64a84ca
guderian120 <realamponsah10@yahoo.com>
2025-05-29 08:34:48 +0000

    debug push, removed local profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,66 +1,36 @@
-# Configure AWS provider (replace with your region)
+# Configure AWS provider
 provider "aws" {
-  region = "eu-west-1"  
-
-  # profile = "sandbox"  # Use your AWS CLI profile
+  region = "eu-west-1"
+  # profile = "sandbox"  # Uncomment if using named profile
 }
 
+# Reference an existing IAM role by name
 data "aws_iam_role" "lambda_role" {
   name = "lambda-execution-role"
 }
 
-
+# Attach AWS-managed policy to the existing IAM role
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = data.aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-# Create Lambda function from S3 ZIP
+
+# Create Lambda function: create_task
 resource "aws_lambda_function" "create_task" {
-  function_name = "create_task"  # Name of the Lambda function
-  handler       = "create_task.lambda_handler"  # <filename>.<handler_name>
+  function_name = "create_task"
+  handler       = "create_task.lambda_handler"
   runtime       = "python3.12"
-  role          = aws_iam_role.lambda_role.arn  # Create this role (see below)
+  role          = data.aws_iam_role.lambda_role.arn  # Use the existing role
   s3_bucket     = "lambdacicdtest0123"
-  s3_key        = "lambda-zips/create_task.zip"  # Path to your ZIP in S3
-
-  # Optional: Ensure Lambda updates when ZIP changes
-#   source_code_hash = filebase64sha256("${path.module}./src/create_task/function.zip")
+  s3_key        = "lambda-zips/create_task.zip"
 }
+
+# Create Lambda function: delete_task
 resource "aws_lambda_function" "delete_task" {
-  function_name = "delete_task"  # Name of the Lambda function
-  handler       = "delete_task.lambda_handler"  # <filename>.<handler_name>
+  function_name = "delete_task"
+  handler       = "delete_task.lambda_handler"
   runtime       = "python3.12"
-  role          = aws_iam_role.lambda_role.arn  # Create this role (see below)
+  role          = data.aws_iam_role.lambda_role.arn  # Use the existing role
   s3_bucket     = "lambdacicdtest0123"
-  s3_key        = "lambda-zips/delete_task.zip"  # Path to your ZIP in S3
-
-  # Optional: Ensure Lambda updates when ZIP changes
-#   source_code_hash = filebase64sha256("${path.module}./src/create_task/function.zip")
+  s3_key        = "lambda-zips/delete_task.zip"
 }
-
-# # Minimal IAM role for Lambd
-# resource "aws_iam_role" "lambda_role" {
-#   name = "lambda-execution-role"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "lambda.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
-
-# # Allow Lambda to write log
-# resource "aws_iam_role_policy_attachment" "lambda_logs" {
-#   role       = aws_iam_role.lambda_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
-
-
-
-
-
