3a4ab68f2944a500fcec2ac4c26e91174c6ddd52
guderian120 <realamponsah10@yahoo.com>
2025-05-29 08:03:38 +0000

    debug push

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "delete_task" {
 #   source_code_hash = filebase64sha256("${path.module}./src/create_task/function.zip")
 }
 
-# Minimal IAM role for Lambda
+# Minimal IAM role for Lambd
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-execution-role"
 
