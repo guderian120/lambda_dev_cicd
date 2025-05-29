566ef26f544059a3a1bcc98416ecfe839d755c9c
guderian120 <realamponsah10@yahoo.com>
2025-05-29 08:24:17 +0000

    debug push, removed local profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# Allow Lambda to write logs
+# Allow Lambda to write log
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
