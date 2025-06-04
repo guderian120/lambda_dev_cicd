79637c67e13dac44a70749c425ed7fd7bb4458db
guderian120 <realamponsah10@yahoo.com>
2025-06-04 09:00:57 +0000

    added production functions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # Configure AWS provider
 provider "aws" {
   region = "eu-west-1"
-  # profile = "sandbox"  # Uncomment if using named profile
+  profile = "sandbox"  # Uncomment if using named profile
 }
 
 # Reference an existing IAM role by name
@@ -16,14 +16,14 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 }
 
 # Create Lambda function: create_task
-resource "aws_lambda_function" "create_task" {
-  function_name = "create_task"
-  handler       = "create_task.lambda_handler"
-  runtime       = "python3.12"
-  role          = data.aws_iam_role.lambda_role.arn  # Use the existing role
-  s3_bucket     = "lambdacicdtest0123"
-  s3_key        = "lambda-zips/create_task.zip"
-}
+  resource "aws_lambda_function" "create_task" {
+    function_name = "create_task"
+    handler       = "create_task.lambda_handler"
+    runtime       = "python3.12"
+    role          = data.aws_iam_role.lambda_role.arn  # Use the existing role
+    s3_bucket     = "lambdacicdtest0123"
+    s3_key        = "lambda-zips/create_task.zip"
+  }
 
 # Create Lambda function: delete_task
 resource "aws_lambda_function" "delete_task" {
